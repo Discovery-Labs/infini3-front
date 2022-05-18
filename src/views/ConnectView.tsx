f82301225514846b9e07ed034aec1a62902bee78
aeither <36173828+aeither@users.noreply.github.com>
2022-05-18 23:40:24 +0200

    feat: connect wallet view

--- src/views/ConnectView.tsx
+++ src/views/ConnectView.tsx
@@ -1,24 +1,59 @@
 // Set the value
 import { Button, Heading, Stack, Text, VStack } from "@chakra-ui/react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import useStore, { AdventureEnum } from "core/states";
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import { useConnect } from "wagmi";
 
 const ConnectView = () => {
-  const { adventureState, setAdventureState } = useStore();
+  const [playYay] = useSound("/sounds/yay.mp3");
+  const [playClick] = useSound("/sounds/click.mp3");
+  const { setProgress, setAdventureState } = useStore();
+  const { activeConnector } = useConnect();
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  useEffect(() => {
+    setIsCompleted(activeConnector ? true : false);
+  }, [activeConnector]);
+
+  useEffect(() => {
+    if (isCompleted) {
+      playYay();
+      setProgress(10);
+    }
+  }, [isCompleted]);
+
   return (
     <VStack h="inherit" justify="space-between" maxW="xl">
       <Heading pt="4" textAlign="center">
         Connect your wallet
       </Heading>
-      <Stack layerStyle="solid-card">
-        <Text>Connect your wallet : {adventureState}</Text>
+
+      {activeConnector ? (
+        <Stack layerStyle="solid-card" align="center" spacing={8}>
+          <Text>Connected to {activeConnector.name}</Text>
+        </Stack>
+      ) : (
+        <Stack layerStyle="solid-card" align="center" spacing={8}>
+          <Text>Connect your wallet</Text>
+          <ConnectButton />
+        </Stack>
+      )}
+
+      <Stack w="full" bg="accent">
+        {isCompleted && <Text>Nicely done</Text>}
+        <Button
+          w="full"
+          disabled={!isCompleted}
+          onClick={() => {
+            playClick();
+            setAdventureState(AdventureEnum.mintAdventurer);
+          }}
+        >
+          Next
+        </Button>
       </Stack>
-      <Button
-        w="full"
-        onClick={() => setAdventureState(AdventureEnum.mintAdventurer)}
-      >
-        Next
-      </Button>
     </VStack>
   );
 };
