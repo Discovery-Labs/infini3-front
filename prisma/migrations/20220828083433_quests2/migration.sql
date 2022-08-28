c882b0771ca529e4d76b887a52358ad7667f7b7b
aeither <36173828+aeither@users.noreply.github.com>
2022-08-28 13:54:47 +0200

    add schema

---
+++ prisma/migrations/20220828083433_quests2/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `title` on the `quests` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "quests" DROP COLUMN "title";
