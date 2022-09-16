f04abd27cbb78c61586e7463bcfef3637d58133d
aeither <36173828+aeither@users.noreply.github.com>
2022-09-16 16:14:19 +0200

    gen bounty id function

--- src/pages/api/mint-badge.tsx
+++ src/pages/api/mint-badge.tsx
@@ -48,6 +48,19 @@ const mintBadge = async (req: NextApiRequest, res: NextApiResponse) => {
   const { tokenId, questsId, experiencePoints } = JSON.parse(req.body);
   const userId = req.cookies.cuid;
 
+  function makeBountyId(length: number) {
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  console.log(makeBountyId(6));
+
   try {
     await prisma.quests_users.create({
       data: {
